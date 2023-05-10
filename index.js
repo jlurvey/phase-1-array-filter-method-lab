e80eaa2f534af0858c0ce224076b65e283effa52
Joseph Lurvey <joeylurvey@gmail.com>
2023-05-09 20:55:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array,string) {
+    const matchFind = array.filter(function (name) {
+        return name.toLowerCase() === string.toLowerCase()
+    })
+    return matchFind;
+};
+
+function fuzzyMatch(array, string) {
+    const fuzzyFind = array.filter(function (letters) {
+        return letters[0] === string[0]
+    });
+    return fuzzyFind;
+};
+
+function matchName(array,string) {
+    const nameMatch = array.filter(function (obj) {
+        return obj.name === string
+    });
+        return nameMatch;
+};
